6310a6a9d97512e46e8512dca182454a86116ed0
Thomas Hoque <K31361@eltham-college.org.uk>
2020-03-04 15:35:14 +0000

    New save

--- LoginProject/Program.cs
+++ LoginProject/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-
+using System.IO;
 namespace LoginProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Would you like to login or signout?");
+            string UInput;
+            UInput = Console.ReadLine();
+            if (UInput == "Register")
+
+            {
+                Console.WriteLine("Type in username");
+                string username;
+                username = Console.ReadLine();
+                Console.WriteLine("Type in password");
+                string password;
+                password = Console.ReadLine();
+
+                string user = $"{username}, {password}";
+
+                StreamWriter tjh = null; ;
+                try
+                {
+                    tjh = new StreamWriter("C:/Users/K31361/Register.txt");
+                    tjh.WriteLine(user);
+
+                    tjh.Close();
+
+                }
+                catch
+                {
+                    Console.WriteLine("error");
+                }
+                
+
+            }
+            else
+            {
+                Console.WriteLine("Type in your username");
+                Console.WriteLine("Type in your password");
+            }
         }
     }
 }
+
+
